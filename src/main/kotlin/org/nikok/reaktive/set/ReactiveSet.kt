37ff8b4b9a3a2e7285b21c3954363aea4aad8f85
NKb03 <niko.knop003@gmail.com>
2018-09-25 15:32:10 +0200

    Override function signatures for transformations to return ReactiveSets

--- src/main/kotlin/org/nikok/reaktive/set/ReactiveSet.kt
+++ src/main/kotlin/org/nikok/reaktive/set/ReactiveSet.kt
@@ -1,8 +1,26 @@
 package org.nikok.reaktive.set
 
+import org.nikok.reaktive.Observer
+import org.nikok.reaktive.collection.CollectionChangeHandler
 import org.nikok.reaktive.collection.ReactiveCollection
 
 /**
  * A Set of elements which can be observed for modifications
 */
-interface ReactiveSet<E> : ReactiveCollection<E, SetChange<E>>
+interface ReactiveSet<out E> : ReactiveCollection<E, SetChange<E>> {
+    override fun observe(handler: CollectionChangeHandler<E, SetChange<E>>): Observer 
+
+    override fun <F> map(newName: String, f: (E) -> F): ReactiveSet<F>
+
+    override fun filter(newName: String, predicate: (E) -> Boolean): ReactiveSet<E>
+
+    override fun <F> flatMap(newName: String, f: (E) -> ReactiveCollection<F, *>): ReactiveSet<F>
+
+    override fun minus(other: Collection<@UnsafeVariance E>): ReactiveSet<E>
+
+    override fun minus(other: ReactiveCollection<@UnsafeVariance E, *>): ReactiveSet<E>
+
+    override fun plus(other: Collection<@UnsafeVariance E>): ReactiveSet<E>
+
+    override fun plus(other: ReactiveCollection<@UnsafeVariance E, *>): ReactiveSet<E>
+}
