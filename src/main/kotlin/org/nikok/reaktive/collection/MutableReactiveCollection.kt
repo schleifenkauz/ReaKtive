cf7cf1cddc7756919b783add374086b8bdce765d
NKb03 <niko.knop003@gmail.com>
2018-09-25 16:12:09 +0200

    Add writer property to MutableReactiveCollection

--- src/main/kotlin/org/nikok/reaktive/collection/MutableReactiveCollection.kt
+++ src/main/kotlin/org/nikok/reaktive/collection/MutableReactiveCollection.kt
@@ -2,4 +2,10 @@ package org.nikok.reaktive.collection
 
 interface MutableReactiveCollection<E, C : CollectionChange<E>> : ReactiveCollection<E, C> {
     override val now: MutableCollection<E>
+
+    /**
+     * @return a [CollectionWriter] which holds only a [java.lang.ref.WeakReference] to this [MutableReactiveCollection]
+     * when there are no handlers observing this collection
+    */
+    val writer: CollectionWriter<E>
 }
