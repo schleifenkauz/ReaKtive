e5b7a16faf13b41f4dec4a0c44a808646bd469d9
NKb03 <niko.knop003@gmail.com>
2018-09-25 15:28:42 +0200

    Create transformation bindings

--- src/main/kotlin/org/nikok/reaktive/collection/ReactiveCollection.kt
+++ src/main/kotlin/org/nikok/reaktive/collection/ReactiveCollection.kt
@@ -6,6 +6,7 @@ package org.nikok.reaktive.collection
 
 import org.nikok.reaktive.Observer
 import org.nikok.reaktive.Reactive
+import org.nikok.reaktive.value.ReactiveValue
 
 /**
  * A [Collection] of elements of type [E] which can be observed for changes of type [C]
@@ -22,14 +23,43 @@ interface ReactiveCollection<out E, out C : CollectionChange<E>> : Reactive {
     */
     fun observe(handler: CollectionChangeHandler<E, C>): Observer
 
-    //Transformations
     /**
-     * @return
+     * @return A [ReactiveCollection] named [newName] which contains all elements from this collection mapped with [f]
     */
     fun <F> map(newName: String, f: (E) -> F): ReactiveCollection<F, *>
 
     /**
-     * @return
+     * @return A [ReactiveCollection] named [newName] which contains all elements
+     * from this collection which satisfy the given [predicate]
     */
-    fun filter(newName: String, f: (E) -> Boolean)
+    fun filter(newName: String, predicate: (E) -> Boolean): ReactiveCollection<E, C>
+
+    /**
+     * @return a flat view on this collection mapped with [f] named [newName]
+     */
+    fun <F> flatMap(newName: String, f: (E) -> ReactiveCollection<F, *>): ReactiveCollection<F, *>
+
+    /**
+     * @return a [ReactiveCollection] containing all elements from this collection not contained in [other]
+    */
+    fun minus(other: Collection<@UnsafeVariance E>): ReactiveCollection<E, C>
+
+    /**
+     * @return a [ReactiveCollection] containing all elements from this collection not contained in [other]
+    */
+    fun minus(other: ReactiveCollection<@UnsafeVariance E, *>): ReactiveCollection<E, C>
+    /**
+     * @return a [ReactiveCollection] containing all elements contained in this or in the [other] [Collection]
+    */
+    fun plus(other: Collection<@UnsafeVariance E>): ReactiveCollection<E, C>
+
+    /**
+     * @return a [ReactiveCollection] containing all elements contained in this or in the [other] [ReactiveCollection]
+    */
+    fun plus(other: ReactiveCollection<@UnsafeVariance E, *>): ReactiveCollection<E, C>
+
+    /**
+     * @return a [ReactiveValue] folding the elements of this [ReactiveCollection]
+    */
+    fun <T> fold(name: String, initial: T, op: (T, E) -> T): ReactiveValue<T>
 }
