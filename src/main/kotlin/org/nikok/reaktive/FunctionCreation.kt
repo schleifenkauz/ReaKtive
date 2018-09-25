679ecdba6c57eed5615596acc10b5b74dadf4c94
NKb03 <niko.knop003@gmail.com>
2018-09-25 16:25:36 +0200

    Add invalidationHandler and asInvalidationHandler

--- src/main/kotlin/org/nikok/reaktive/FunctionCreation.kt
+++ src/main/kotlin/org/nikok/reaktive/FunctionCreation.kt
@@ -1,8 +1,23 @@
 package org.nikok.reaktive
 
+import kotlin.reflect.KFunction1
+
 /**
  * @author Nikolaus Knop
  */
+
+/**
+ * @return an [InvalidationHandler] described by [desc] which uses [handle] to handle invalidation
+ */
+fun <T> invalidationHandler(desc: String, handle: (Reactive) -> T): InvalidationHandler = handle.withDescription(desc)
+
+/**
+ * @return a [InvalidationHandler] using the name of the reflected function
+ * @throws IllegalArgumentException if the receiver is an anonymous function
+ */
+fun <T> (KFunction1<Reactive, T>).asInvalidationHandler(): InvalidationHandler {
+    return invalidationHandler(name, this)
+}
 
 /**
  * @return an [InvalidationHandler] described by [desc] which uses the receiver function to handle invalidation
