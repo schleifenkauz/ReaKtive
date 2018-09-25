45830bb79e91d2944fccbc9b929043780aefe093
NKb03 <niko.knop003@gmail.com>
2018-09-25 15:37:30 +0200

    Add reactive extensions to wrap mutable sets in reactive sets

--- src/main/kotlin/org/nikok/reaktive/set/create.kt
+++ src/main/kotlin/org/nikok/reaktive/set/create.kt
@@ -7,9 +7,14 @@ package org.nikok.reaktive.set
 /**
  * @return a [ReactiveSet] containing [elements]
  */
-fun <E> reactiveSetOf(vararg elements: E): ReactiveSet<E> = TODO()
+fun <E> reactiveSetOf(vararg elements: E): MutableReactiveSet<E> = TODO()
 
 /**
  * @return a [ReactiveSet] consisting of the elements in the receiver
  */
-fun <E> Iterable<E>.toReactiveSet(): ReactiveSet<E> = TODO()
+fun <E> Iterable<E>.toReactiveSet(): MutableReactiveSet<E> = TODO()
+
+/**
+ * @return a [MutableReactiveSet] wrapping the [MutableSet]
+*/
+fun <E> MutableSet<E>.reactive(): MutableReactiveSet<E> = TODO()
