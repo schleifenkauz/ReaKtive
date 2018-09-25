31a4d4785ca9a76077078569bb0feb6f41f12e01
NKb03 <niko.knop003@gmail.com>
2018-09-25 16:11:52 +0200

    Add CollectionWriter

---
+++ src/main/kotlin/org/nikok/reaktive/collection/CollectionWriter.kt
@@ -0,0 +1,40 @@
+package org.nikok.reaktive.collection
+
+/**
+ * Used to modify [ReactiveCollection]s
+*/
+interface CollectionWriter<E> {
+    /**
+     * Add the specified [element] to the associated collection
+     * @return whether the element was added
+    */
+    fun add(element: E): Boolean
+
+    /**
+     * Remove the specified [element] from the associated collection
+     * @return whether the element was removed
+     */
+    fun remove(element: E): Boolean
+
+    /**
+     * Add all [elements] to the associated collection
+     * @return whether any elements were added
+     */
+    fun addAll(elements: Collection<E>): Boolean
+
+    /**
+     * Remove all [elements] from the associated collection
+     * @return whether any elements were removed
+    */
+    fun removeAll(elements: Collection<E>)
+
+    /**
+     * Clear the associated collection
+    */
+    fun clear()
+
+    /**
+     * Execute [block] feeding the current content of the collection to it when it wasn't garbage collected before
+    */
+    fun <T> withContent(block: (MutableCollection<E>) -> T): T
+}
