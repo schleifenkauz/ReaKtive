22bb6fd3f29f4c8ca49a71b1571dd61ee0ea7487
NKb03 <niko.knop003@gmail.com>
2018-09-25 17:18:59 +0200

    Allow creating unmodifiable reactive sets

--- src/main/kotlin/org/nikok/reaktive/set/create.kt
+++ src/main/kotlin/org/nikok/reaktive/set/create.kt
@@ -4,6 +4,8 @@
 
 package org.nikok.reaktive.set
 
+import org.nikok.reaktive.set.impl.UnmodifiableReactiveSet
+
 /**
  * @return a [ReactiveSet] containing [elements]
  */
@@ -16,5 +18,11 @@ fun <E> Iterable<E>.toReactiveSet(): MutableReactiveSet<E> = TODO()
 
 /**
  * @return a [MutableReactiveSet] wrapping the [MutableSet]
-*/
+ */
 fun <E> MutableSet<E>.reactive(): MutableReactiveSet<E> = TODO()
+
+/**
+ * @return an unmodifiable set containing the specified [elements]
+ */
+fun <E> unmodifiableReactiveSet(description: String, elements: Iterable<E>): ReactiveSet<E> =
+        UnmodifiableReactiveSet(description, elements)
