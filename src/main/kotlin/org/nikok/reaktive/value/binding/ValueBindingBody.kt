fd25739efef3779d9f578b48196294098f8feb00
NKb03 <niko.knop003@gmail.com>
2018-09-26 16:39:08 +0200

    factory out binding body

--- src/main/kotlin/org/nikok/reaktive/value/binding/ValueBindingBody.kt
+++ src/main/kotlin/org/nikok/reaktive/value/binding/ValueBindingBody.kt
@@ -4,6 +4,7 @@
 
 package org.nikok.reaktive.value.binding
 
+import org.nikok.reaktive.BindingBody
 import org.nikok.reaktive.Observer
 import org.nikok.reaktive.value.ReactiveValue
 import org.nikok.reaktive.value.ReactiveVariable
@@ -13,7 +14,7 @@ import org.nikok.reaktive.value.ReactiveVariable
  * * Only uses the [ReactiveVariable.setter] of the associated [ReactiveVariable] meaning that it could be garbage collected
  * when they are no observers or strong references anymore to it
  */
-interface BindingBody<T> {
+interface ValueBindingBody<T>: BindingBody {
     /**
      * Sets the value of the associated [Binding] to [value]
      */
@@ -29,9 +30,4 @@ interface BindingBody<T> {
      * Executes [use] feeding the current value to it only if the associated [Binding] was not garbage collected before
      */
     fun withValue(use: (value: T) -> Unit)
-
-    /**
-     * Prevents [observer] from being garbage collected until the associated [Binding] is finalized
-     */
-    fun addObserver(observer: Observer)
 }
