f590437aa9e7a8225e61e6377fa5182384086494
Nikolaus Knop <niko.knop003@gmail.com>
2019-01-17 16:15:53 +0100

    Remove unneeded dsl components and fix ExistenceBindingsSpec

--- src/test/kotlin/reaktive/collection/bindings/ExistenceBindingsSpec.kt
+++ src/test/kotlin/reaktive/collection/bindings/ExistenceBindingsSpec.kt
@@ -6,16 +6,18 @@ import reaktive.collection.size
 import reaktive.set.reactiveSet
 import reaktive.value.binding.testBinding
 
-internal object ExistenceBindingsSpec: Spek({
+internal object ExistenceBindingsSpec : Spek({
     val collection: MutableReactiveCollection<Int> = reactiveSet()
     val size = collection.size
     fun expected() = collection.now.size
     testBinding(size, ::expected) {
-        collection.now.add(1)
-        collection.now.remove(1)
-        collection.now.addAll(setOf(1, 2, 3, 4, 5))
-        collection.now.addAll(setOf(2, 4, 8))
-        collection.now.removeAll(setOf(234, 34, 234, 3, 5, 1))
-        collection.now.clear()
+        with(collection.now) {
+            "add an element" { add(1) }
+            "remove an element" { remove(1) }
+            "add 5 elements" { addAll(setOf(1, 2, 3, 4, 5)) }
+            "add 1 new element" { addAll(setOf(2, 4, 8)) }
+            "remove 3 elements" { removeAll(setOf(234, 34, 234, 3, 5, 1)) }
+            "clear the set" { clear() }
+        }
     }
 })
--- src/test/kotlin/reaktive/value/binding/dsl.kt
+++ src/test/kotlin/reaktive/value/binding/dsl.kt
@@ -4,47 +4,10 @@
 
 package reaktive.value.binding
 
-import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.should.shouldMatch
 import org.jetbrains.spek.api.dsl.*
-import reaktive.Reactive
 import reaktive.value.now
-
-internal inline fun <T> testBinding(
-    dependencies: Reactive,
-    binding: Binding<T>,
-    crossinline check: (T) -> Unit,
-    actions: () -> Unit
-) {
-    val obs = dependencies.observe { check(binding.now) }
-    actions()
-    obs.kill()
-}
-
-@JvmName("testBindingWithCriteria")
-internal inline fun <T> Spec.testBinding(
-    dependencies: Reactive,
-    binding: Binding<T>,
-    crossinline criteria: () -> Matcher<T>,
-    actions: () -> Unit
-) = testBinding(dependencies, binding, { now ->
-    test("it should update correctly") {
-        now shouldMatch criteria()
-    }
-}, actions)
-
-internal inline fun <T> Spec.testBinding(
-    dependencies: Reactive,
-    binding: Binding<T>,
-    crossinline expected: () -> T,
-    actions: () -> Unit
-) = testBinding(
-    dependencies,
-    binding,
-    { now -> it("should update correctly") { now shouldMatch equalTo(expected()) } },
-    actions
-)
 
 internal inline fun <T> Spec.testBinding(
     binding: Binding<T>,
