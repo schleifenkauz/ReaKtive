1eb0bb7fed6d6dd6c3e91a87582a8f183d9869a8
Nikolaus Knop <niko.knop003@gmail.com>
2019-01-20 09:46:25 +0100

    Use priority queue for minimum element binding

--- src/main/kotlin/reaktive/collection/binding/compare.kt
+++ src/main/kotlin/reaktive/collection/binding/compare.kt
@@ -8,19 +8,24 @@ import reaktive.collection.ReactiveCollection
 import reaktive.value.ReactiveValue
 import reaktive.value.binding.Binding
 import reaktive.value.binding.binding
+import java.util.*
 
-fun <E> ReactiveCollection<E>.minWith(comparator: Comparator<E>): Binding<E?> = binding(now.minWith(comparator)) {
+fun <E : Any> ReactiveCollection<E>.minWith(comparator: Comparator<E>): Binding<E?> = binding<E?>(null) {
+    val queue = PriorityQueue(now.size, comparator)
+    queue.addAll(now)
     val obs = observeCollection { ch ->
-        if (ch.wasRemoved)
-            withValue { v -> if (ch.element == v) set(now.minWith(comparator)) }
-        else if (ch.wasAdded) withValue { v ->
-            if (v == null || comparator.compare(ch.element, v) > 0) set(ch.element)
+        if (ch.wasRemoved) {
+            queue.remove(ch.element)
+            withValue { v -> if (ch.element == v) set(queue.peek()) }
+        } else if (ch.wasAdded) withValue { v ->
+            queue.add(ch.element)
+            if (v == null || comparator.compare(ch.element, v) < 0) set(ch.element)
         }
     }
     addObserver(obs)
 }
 
-fun <E, C : Comparable<C>> ReactiveCollection<E>.minBy(selector: (E) -> C): Binding<E?> =
+fun <E : Any, C : Comparable<C>> ReactiveCollection<E>.minBy(selector: (E) -> C): Binding<E?> =
     minWith(Comparator.comparing(selector))
 
 inline fun <E, C : Comparable<C>> ReactiveCollection<E>.minByR(selector: (E) -> ReactiveValue<C>): Binding<E?> =
