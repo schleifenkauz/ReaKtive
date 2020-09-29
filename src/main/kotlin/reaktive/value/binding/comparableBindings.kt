18163fcaf4c9d7bf31411c2f64f510210b7c9571
Nikolaus Knop <niko.knop003@gmail.com>
2020-09-29 15:01:05 +0200

    Add explicit type signatures to some bindings

--- src/main/kotlin/reaktive/value/binding/comparableBindings.kt
+++ src/main/kotlin/reaktive/value/binding/comparableBindings.kt
@@ -4,15 +4,7 @@
 
 package reaktive.value.binding
 
-import reaktive.dependencies
 import reaktive.value.ReactiveValue
-import reaktive.value.now
-
-fun <T, F> binding(
-    op1: ReactiveValue<T>,
-    op2: ReactiveValue<T>,
-    operator: (T, T) -> F
-) = binding<F>(dependencies(op1, op2)) { operator(op1.now, op2.now) }
 
 fun <C : Comparable<O>, O> ReactiveValue<C>.compareTo(other: O): Binding<Int> =
     map { v -> v.compareTo(other) }
--- src/main/kotlin/reaktive/value/binding/utility.kt
+++ src/main/kotlin/reaktive/value/binding/utility.kt
@@ -7,20 +7,20 @@ package reaktive.value.binding
 import reaktive.dependencies
 import reaktive.value.*
 
-fun <T> ReactiveValue<T>.equalTo(other: ReactiveValue<T>) =
+fun <T> ReactiveValue<T>.equalTo(other: ReactiveValue<T>): Binding<Boolean> =
     binding<Boolean>(dependencies(this, other)) { this.now == other.now }
 
-fun <T> ReactiveValue<T>.equalTo(other: T) = map { it == other }
+fun <T> ReactiveValue<T>.equalTo(other: T): Binding<Boolean> = map { it == other }
 
-fun <T> ReactiveValue<T>.notEqualTo(other: ReactiveValue<T>) =
+fun <T> ReactiveValue<T>.notEqualTo(other: ReactiveValue<T>): Binding<Boolean> =
     binding<Boolean>(dependencies(this, other)) { this.now != other.now }
 
-fun <T> ReactiveValue<T>.notEqualTo(other: T) = map { it != other }
+fun <T> ReactiveValue<T>.notEqualTo(other: T): Binding<Boolean> = map { it != other }
 
-fun <T> ReactiveValue<T>.takeIf(pred: (T) -> Boolean) = map { it.takeIf(pred) }
+fun <T> ReactiveValue<T>.takeIf(pred: (T) -> Boolean): Binding<T?> = map { it.takeIf(pred) }
 
-fun <T> ReactiveValue<T>.takeIf(boolean: ReactiveBoolean) = boolean.map { if (it) now else null }
+fun <T> ReactiveValue<T>.takeIf(boolean: ReactiveBoolean): Binding<T?> = boolean.map { if (it) now else null }
 
-fun <T> ReactiveValue<T?>.orElse(then: ReactiveValue<T>) = binding(this, then) { a, b -> a ?: b }
+fun <T> ReactiveValue<T?>.orElse(then: ReactiveValue<T>): Binding<T> = binding(this, then) { a, b -> a ?: b }
 
-fun <T> ReactiveValue<T?>.orElse(then: T) = map { it ?: then }
+fun <T> ReactiveValue<T?>.orElse(then: T): Binding<T> = map { it ?: then }
