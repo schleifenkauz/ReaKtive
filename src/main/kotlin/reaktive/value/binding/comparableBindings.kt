9373f4b7462ff7bbba845fac28e087365def37ca
Nikolaus Knop <niko.knop003@gmail.com>
2025-06-18 00:50:25 +0200

    Add some more `comparableBindings`

--- src/main/kotlin/reaktive/value/binding/comparableBindings.kt
+++ src/main/kotlin/reaktive/value/binding/comparableBindings.kt
@@ -6,8 +6,14 @@ package reaktive.value.binding
 
 import reaktive.value.ReactiveValue
 
-fun <C : Comparable<O>, O> ReactiveValue<C>.compareTo(other: O): Binding<Int> =
-    map { v -> v.compareTo(other) }
+fun <C : Comparable<O>, O> ReactiveValue<C>.compareTo(other: O): Binding<Int> = map { v -> v.compareTo(other) }
 
-fun <C : Comparable<O>, O> ReactiveValue<C>.greaterThan(other: O): Binding<Boolean> =
-    compareTo(other).map { it > 0 }
+fun <C : Comparable<O>, O> ReactiveValue<C>.greaterThan(other: O): Binding<Boolean> = compareTo(other).map { it > 0 }
+
+fun <C : Comparable<O>, O> ReactiveValue<C>.lessThan(other: O): Binding<Boolean> = compareTo(other).map { it < 0 }
+
+fun <C : Comparable<O>, O> ReactiveValue<C>.greaterThanOrEqualTo(other: O): Binding<Boolean> =
+    compareTo(other).map { it >= 0 }
+
+fun <C : Comparable<O>, O> ReactiveValue<C>.lessThanOrEqualTo(other: O): Binding<Boolean> =
+    compareTo(other).map { it <= 0 }
