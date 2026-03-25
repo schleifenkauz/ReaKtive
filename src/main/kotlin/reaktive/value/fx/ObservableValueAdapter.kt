df901dd9aaf9b29b2278b65937114e122320d643
Nikolaus Knop <niko.knop003@gmail.com>
2026-03-25 21:28:26 +0100

    Wrap JavaFX `listener.changed` calls in `Platform.runLater` to ensure application thread execution

--- src/main/kotlin/reaktive/value/fx/ObservableValueAdapter.kt
+++ src/main/kotlin/reaktive/value/fx/ObservableValueAdapter.kt
@@ -23,15 +23,45 @@ internal open class ObservableValueAdapter<T>(private val wrapped: ReactiveValue
 
     override fun addListener(listener: ChangeListener<in T>) {
         val obs = wrapped.observe { _, old, new ->
-            listener.changed(this, old, new)
+            runLater { listener.changed(this, old, new) }
         }
         listenersToObservers[listener] = obs
     }
 
     override fun addListener(listener: InvalidationListener) {
-        val obs = wrapped.observe { _, _, _ -> listener.invalidated(this) }
+        val obs = wrapped.observe { _, _, _ ->
+            runLater { listener.invalidated(this) }
+        }
         listenersToObservers[listener] = obs
     }
 
     override fun getValue(): T = wrapped.get()
+
+    companion object {
+        val platformCls by lazy {
+            try {
+                ObservableValueAdapter::class.java.classLoader.loadClass("javafx.application.Platform")
+            } catch (e: ClassNotFoundException) {
+                null
+            } catch (e: SecurityException) {
+                null
+            }
+        }
+
+        private val isFxApplicationThreadMethod by lazy {
+            platformCls?.getDeclaredMethod("isFxApplicationThread")
+        }
+
+        private val runLaterMethod by lazy {
+            platformCls?.getDeclaredMethod("runLater", Runnable::class.java)
+        }
+
+        private fun runLater(action: Runnable) {
+            when {
+                isFxApplicationThreadMethod?.invoke(null) == true -> action.run()
+                runLaterMethod == null -> action.run()
+                else -> runLaterMethod!!.invoke(null, action)
+            }
+        }
+    }
 }
